f7bfff143a8e72ecff3664387902122dc6177d86
mihir <phatak.mihir@gmail.com>
2019-08-27 18:44:55 -0400

    LCA of BST

---
+++ 235-LCA-of-BST/same_as_BT_Expensive_solution.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        
+        TreeNode* ancl = NULL;
+        TreeNode* ancr = NULL;
+        
+        if(!root)
+        {
+            return NULL;
+        }
+        
+        if(root == p || root == q)
+        {
+            return root;
+        }
+        
+        ancl = lowestCommonAncestor(root->left, p, q);
+        ancr = lowestCommonAncestor(root->right, p, q); 
+        
+        if(!ancl)
+        {
+            return ancr;
+        }
+        if(!ancr)
+        {
+            return ancl;
+        }
+        
+        return root;
+        
+    }
+};

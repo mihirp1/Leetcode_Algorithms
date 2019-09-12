82dfbfd7de4cd80fa958a2ea9a55367282d90a82
mihir <phatak.mihir@gmail.com>
2019-09-11 20:27:01 -0400

    56 : For if else clauses simplified

---
+++ 56-merge-intervals/slight-simplified-for-if-else.cpp
@@ -0,0 +1,39 @@
+//https://leetcode.com/problems/merge-intervals/
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& vec) {
+        
+        //std::vector<std::vector<int>> final_array;
+        
+        
+        if(vec.size()== 0)
+        {
+            return vec;
+        }
+        
+        std::sort(vec.begin(),vec.end());
+        
+
+        std::vector<vector<int>> res;
+        res.push_back(vec[0]);
+        
+        for(int i = 1 ; i < vec.size(); ++i)
+        {
+                if(vec[i][0] > res[res.size()-1][1]) 
+                {
+                    res.push_back(vec[i]);
+                
+                }
+                else if(vec[i][1] > res[res.size()-1][1])
+                {
+                    res[res.size()-1][1] = vec[i][1];
+                
+                }
+        }
+        
+        
+        
+        return res;
+        
+    }
+};

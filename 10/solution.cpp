d7bcb60559370652b32a92f1341c2e5c8ff66ea2
mihir <phatak.mihir@gmail.com>
2019-09-17 21:27:54 -0400

    Check for subtree

---
+++ 10/solution.cpp
@@ -0,0 +1,52 @@
+//https://leetcode.com/problems/subtree-of-another-tree/
+//https://www.youtube.com/watch?v=73PQ9raLEVs
+//https://www.youtube.com/watch?v=kL5Gs1YTwMM
+//https://leetcode.com/playground/2KqUiidx
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+        
+        if(!t)
+            return true;
+        
+        if(!s)
+            return false;
+        
+        if(identical(s,t))
+            return true;
+        
+
+        return(isSubtree(s->right,t) || isSubtree(s->left,t));
+
+    }
+    
+    bool identical(TreeNode* one, TreeNode* two)
+    {
+        if(!one && !two)
+        {
+            return true;
+        }
+        
+        if(!one || !two)
+        {
+            return false;
+        }
+        
+        return ((one->val == two->val) && identical(one->left,two->left) && identical(one->right,two->right));
+        
+        
+        
+    }
+    
+    
+    
+};

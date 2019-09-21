7931d67b566824b3d96955b795e3f0a87b089ae9
mihir <phatak.mihir@gmail.com>
2019-09-21 17:07:30 -0400

    46 permutations

---
+++ 46-permutations/solution.cpp
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/permutations/
+//https://leetcode.com/problems/permutations/discuss/18239/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partioning)
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> temp;
+        vector<vector<int>> main;
+        permute(temp , main , nums);
+     return main;
+    }
+
+
+    void permute(vector<int>& temp, vector<vector<int>>& main, vector<int>& nums)
+    {
+        if(temp.size() == nums.size())
+        {
+            main.push_back(temp);
+        }
+
+        for(int i = 0 ; i < nums.size() ; ++i)
+        {
+            if(find(temp.begin(), temp.end(),nums[i]) != temp.end())
+                continue;
+
+            temp.push_back(nums[i]);
+            permute(temp , main , nums);
+            temp.erase(temp.end()-1);
+
+        }
+
+
+    }
+
+};
+

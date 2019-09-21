657edd8df3a93d3d61d5fab332b6dde638890896
mihir <phatak.mihir@gmail.com>
2019-09-21 16:09:06 -0400

    90 Subsets 2

---
+++ 90-subsets2/solution.cpp
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/subsets-ii/
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+         vector<vector<int>> main;
+        vector<int> temp;
+        //int mask = 1;
+        //std::set<vector<int>> s;
+        sort(nums.begin(),nums.end());
+        
+     recurse(0, nums, main, temp);   
+        
+     return main;   
+    }
+
+        
+        void recurse(int index, vector<int>& nums, vector<vector<int>>& main, vector<int>& temp)
+        {
+            main.push_back(temp);
+            //temp.erase(temp.begin(),temp.end());
+            
+            for(int i = index ; i < nums.size() ; ++i)
+            {
+                if( i > index && nums[i] == nums[i-1]) continue;
+                
+                temp.push_back(nums[i]);
+                recurse(i+1, nums, main, temp);
+                temp.erase(temp.end()-1);
+                
+                
+            }
+            
+            
+        }
+        
+        
+        
+        
+
+};
